c2ffe93ffe888ba823a78b9ab42da321cf09601e
Alica Haunstetter <haunstetter.alica@gmail.com>
2026-09-14 18:09:19 +0200

    hardcoded book-box and finished css

---
+++ books.js
@@ -0,0 +1,136 @@
+let books = [
+  {
+    name: "Harry Potter and the Chamber of Secrets",
+    author: "J.K. Rowling",
+    likes: 750,
+    liked: false,
+    price: 18.0,
+    publishedYear: 1998,
+    genre: "Fantasy",
+    comments: [
+      {
+        name: "BuchKenner",
+        comment:
+          "Ein spannendes Abenteuer, das mich von Anfang an mitgerissen hat.",
+      },
+      {
+        name: "LeseWurm",
+        comment:
+          "Die Liebesgeschichte war herzergreifend und wunderschön geschrieben.",
+      },
+    ],
+  },
+  {
+    name: "Daisy – Und all deine Geheimnisse werden mir gehören",
+    author: "Melanie Raabe",
+    likes: 1250,
+    liked: true,
+    price: 19.99,
+    publishedYear: 2026,
+    genre: "Thriller",
+    comments: [
+      {
+        name: "Leser123",
+        comment:
+          "Ein faszinierendes Abenteuerbuch, das mich von der ersten Seite an gefesselt hat.",
+      },
+      {
+        name: "Bookworm84",
+        comment:
+          "Eine romantische Geschichte, die mein Herz berührt und mich zum Nachdenken gebracht hat.",
+      },
+      {
+        name: "FantasyFanatic",
+        comment:
+          "Eine spannende Fantasiewelt, die ich nur schwer aus der Hand legen konnte.",
+      },
+      {
+        name: "SciFiGuru",
+        comment:
+          "Ein cleverer Science-Fiction-Roman mit interessanten Zeitreise-Konzepten und Charakteren.",
+      },
+      {
+        name: "NovelLover",
+        comment:
+          "Ein Buch, das voller magischer Überraschungen steckt und mich begeistert hat.",
+      },
+    ],
+  },
+  {
+    name: "Dunkel",
+    author: "Ragnar Jónasson",
+    likes: 980,
+    liked: false,
+    price: 14.5,
+    publishedYear: 2020,
+    genre: "Thriller",
+    comments: [],
+  },
+  {
+    name: "Eckstein – Die Kunst des Schiffbruchs",
+    author: "Pascale Quiviger",
+    likes: 1520,
+    liked: true,
+    price: 22.95,
+    publishedYear: 2022,
+    genre: "Fantasy",
+    comments: [
+      {
+        name: "LeserPeter",
+        comment:
+          "Die Handlung war fesselnd und die Charaktere unglaublich lebendig dargestellt.",
+      },
+      {
+        name: "BookLover21",
+        comment:
+          "Ein romantisches Meisterwerk, das mich tief berührt und bewegt hat.",
+      },
+      {
+        name: "FantasyNerd",
+        comment:
+          "Fantastische Welten und epische Abenteuer - genau mein Geschmack!",
+      },
+      {
+        name: "SciFiEnthusiast",
+        comment:
+          "Die Zeitreise-Elemente waren genial und haben die Story spannend gemacht.",
+      },
+      {
+        name: "ReadingAddict",
+        comment:
+          "Ein unvergessliches Buch, das mich auf eine magische Reise mitgenommen hat.",
+      },
+    ],
+  },
+  {
+    name: "The Adventures of Sherlock Holmes",
+    author: "Sir Arthur Conan Doyle",
+    likes: 1300,
+    liked: true,
+    price: 16.75,
+    publishedYear: 1892,
+    genre: "Krimi",
+    comments: [],
+  },
+  {
+    name: "Twilight",
+    author: "Stephenie Meyer",
+    likes: 890,
+    liked: false,
+    price: 12.3,
+    publishedYear: 2005,
+    genre: "Romantik",
+    comments: [
+      {
+        name: "BücherLiebhaber",
+        comment:
+          "Eine magische Reise durch eine faszinierende Fantasiewelt, absolut fesselnd.",
+      },
+      {
+        name: "Leseratte",
+        comment:
+          "Ein packender Science-Fiction-Roman, der mich zum Nachdenken gebracht hat.",
+      },
+    ],
+  },
+];
