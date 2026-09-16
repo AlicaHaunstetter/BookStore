8e9334474e3b2130b4fc2735a5bf0085ad3d3fa4
Alica Haunstetter <haunstetter.alica@gmail.com>
2026-09-16 13:28:20 +0200

    comments softcode js bookindex commentsindex

--- script.js
+++ script.js
@@ -1,16 +1,41 @@
+let comments = [];
+
 function init() {
+  for (let bookindex = 0; bookindex < books.length; bookindex++) {
+    comments[bookindex] = [];
+  }
   renderBooks();
 }
 
 function renderBooks() {
   let content = document.getElementById("main");
   content.innerHTML = "";
-  for (let i = 0; i < books.length; i++) {
-    content.innerHTML += getBookTemplate(i);
+  for (let bookindex = 0; bookindex < books.length; bookindex++) {
+    content.innerHTML += getBookTemplate(bookindex);
   }
 }
 
-function sendComment() {
-  let commentRef = document.getElementById("comment-input");
-  commentRef.push(commentRef) = commentRef.value
+function sendComment(bookindex) {
+  let inputRef = document.getElementById(`comment-input-${bookindex}`);
+  if (inputRef.value != "") {
+    comments[bookindex].push(inputRef.value);
+    inputRef.value = "";
+    renderComments(bookindex);
+  }
+}
+
+function renderComments(bookindex) {
+  let content = document.getElementById(`comment-table-${bookindex}`);
+  content.innerHTML = "";
+  for (
+    let commentindex = 0;
+    commentindex < comments[bookindex].length;
+    commentindex++
+  ) {
+    content.innerHTML += `
+           <tr>
+              <th>Leser123</th>
+              <td>${comments[bookindex][commentindex]}</td>
+            </tr>`;
+  }
 }
--- templates.js
+++ templates.js
@@ -1,28 +1,28 @@
-function getBookTemplate(index) {
+function getBookTemplate(bookindex) {
   return `<section id="book-box" class="book_box">
-      <h2>${books[index].name}</h2>
-      <h4 id="price" class="price">${books[index].price.toFixed(2)} €</h4>
+      <h2>${books[bookindex].name}</h2>
+      <h4 id="price" class="price">${books[bookindex].price.toFixed(2)} €</h4>
       <div id="image-container" class="image_container">
         <img
-          src="./assets/img/${books[index].isbn}.png"
-          alt="Buchcover von ${books[index].name}"
+          src="./assets/img/${books[bookindex].isbn}.png"
+          alt="Buchcover von ${books[bookindex].name}"
         />
       </div>
 
       <table id="book-details" class="book_details">
         <tr>
           <th>AUTHOR</th>
-          <td>${books[index].author}</td>
+          <td>${books[bookindex].author}</td>
         </tr>
 
         <tr>
           <th>YEAR</th>
-          <td>${books[index].publishedYear}</td>
+          <td>${books[bookindex].publishedYear}</td>
         </tr>
 
         <tr>
           <th>GENRE</th>
-          <td>${books[index].genre}</td>
+          <td>${books[bookindex].genre}</td>
         </tr>
       </table>
 
@@ -30,52 +30,7 @@ function getBookTemplate(index) {
         <h3>Comments</h3>
 
         <div class="comment_scroll">
-          <table id="comment-table" class="comment_table">
-            <tr>
-              <th>Leser123</th>
-              <td>pls don't spoiler</td>
-            </tr>
-
-            <tr>
-              <th>Leser123</th>
-              <td>my fav book cover x3</td>
-            </tr>
-            <tr>
-              <th>Leser123</th>
-              <td>pls don't spoiler</td>
-            </tr>
-
-            <tr>
-              <th>Leser123</th>
-              <td>my fav book cover x3</td>
-            </tr>
-            <tr>
-              <th>Leser123</th>
-              <td>pls don't spoiler</td>
-            </tr>
-
-            <tr>
-              <th>Leser123</th>
-              <td>my fav book cover x3</td>
-            </tr>
-            <tr>
-              <th>Leser123</th>
-              <td>pls don't spoiler</td>
-            </tr>
-
-            <tr>
-              <th>Leser123</th>
-              <td>my fav book cover x3</td>
-            </tr>
-            <tr>
-              <th>Leser123</th>
-              <td>pls don't spoiler</td>
-            </tr>
-
-            <tr>
-              <th>Leser123</th>
-              <td>my fav book cover x3</td>
-            </tr>
+          <table id="comment-table-${bookindex}" class="comment_table">
           </table>
         </div>
         <div class="comment_wrapper">
@@ -83,10 +38,10 @@ function getBookTemplate(index) {
             type="text"
             placeholder="write a comment"
             name="CommentInput"
-            id="comment-input"
+            id="comment-input-${bookindex}"
             class="comment_input"
           />
-        <button id="send-comment" class="send_comment" onclick="sendComment()">
+        <button id="send-comment-${bookindex}" class="send_comment" onclick="sendComment(${bookindex})">
           <img src="./assets/icon/send2.svg" alt="send-button" />
         </button>
         </div>
